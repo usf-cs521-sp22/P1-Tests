391226e364e6c36e7dd4b6bc7cc54975b0de5b57
Matthew Malensek <matt@malensek.net>
2022-03-04 13:21:09 -0800

    Rename tests

--- 05-Output-Formatting-1.sh
+++ 05-Output-Formatting-1.sh
@@ -5,7 +5,7 @@ run_timeout=10
 reference_output=$(cd "${TEST_DIR}/inputs/test-fs"; \
     ${TEST_DIR}/scripts/das ./etc | sort)
 
-test_start "Checks the output against the entire test dataset"
+test_start "Checks the program output for file names, sizes, and access times"
 
 program_output=$(cd "${TEST_DIR}/inputs/test-fs"; ${TEST_DIR}/../da ./etc | sort)
 
